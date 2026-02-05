1f0e61692d1700442d0f81f7c13d00a1aad2b9e5
Alexandra <asjlobaton@gmail.com>
2026-02-05 17:41:36 +0300

    Create odd_occurring2.py

---
+++ odd_occurring2.py
@@ -0,0 +1,38 @@
+# program to find two numbers that are odd occurring 
+
+def printTwoOdd(arr, size):
+
+    #xorof2 will hold xor of the 2 odd occurring numbers
+    xorof2 = arr[0]
+
+    # these will hold 3 odd occurring numbers
+    x = 0
+    y= 0
+
+    # this will hold the rightmost set bot from xorof2
+    setbit = 0
+
+    for i in range(1, size):
+        xorof2 = xorof2 ^ arr[i]
+    
+    setbit = xorof2 & ~(xorof2 - 1)
+
+    # if number is having set bit at location we need then OR it with x else y
+    for i in range(size):
+        if(arr[i] & setbit):
+            x = x ^ arr[i]
+        else:
+            y = y ^ arr[i]
+
+        print("The two ODD elements are", x, "&", y)
+
+# create an empty array 
+arr = []
+
+# take array size and elements as input
+arr_size = int(input("Enter size of the array: "))
+for i in range(0, arr_size):
+    z = int(input("Enter element: "))
+    arr.append(z)
+
+printTwoOdd(arr, arr_size)
